546e2c28fb3ce28f99989f1f426f4eef12b43f58
Hoan Dang <hoandv2020@gmail.com>
2019-07-18 13:40:34 +0700

    forms and ???

---
+++ src/main/java/com/codegym/jwt/config/ResponseMessage.java
@@ -0,0 +1,13 @@
+package com.codegym.jwt.config;
+
+public class ResponseMessage {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
